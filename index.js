18dc2e27fbf93618d4b3290f8a03e861ae53b746
Shivesh <shiveshpandey1996@gmail.com>
2023-07-09 23:21:44 +0530

    thapa technical ->Video-2-> Let, var and const

--- index.js
+++ index.js
@@ -1,6 +1,78 @@
-let poo = "Soni"
-console.log(poo)
-var pooja = "jii"
-console.log(pooja)
-const aviral = "chuuuuu"
-console.log(aviral)
+// let poo = "Soni"
+// console.log(poo)
+// var pooja = "jii"
+// console.log(pooja)
+// const aviral = "chuuuuu"
+// console.log(aviral)
+
+// var name = "Shivesh";
+// name = "Pandey";
+// console.log(name);
+
+// let name = "Shivesh";
+// name = "Pandey";
+// console.log(name);
+
+// const name = "Shivesh";
+// name = "Pandey";
+// console.log(name);
+
+////block scope
+// function modeJS(love){
+//     if (love){
+//         let name  = "Shivesh"
+//         const sname = "Pandey"
+//         console.log("My name is "+name+" "+sname)
+//     }
+// }
+// modeJS(true)
+ 
+// function modeJS(love){
+//     if (love){
+//         let name  = "Shivesh"
+//         const sname = "Pandey"
+//     }
+//     console.log("My name is "+name+" "+sname) //error: sname is not defined
+// }
+// modeJS(true)
+ 
+
+// //function scope
+// function modeJS(love){
+//     if (love){
+//         var name  = "Shivesh"
+//         var sname = "Pandey"
+//     }
+//     console.log("My name is "+name+" "+sname)
+// }
+// modeJS(true)
+
+// var whoWillWinToday = "India";
+// if (true){
+//     var whoWillWinToday = "NewZeland";
+//     console.log(whoWillWinToday)
+// }
+// console.log(whoWillWinToday)
+
+// // o/p: NewZeland
+// //      NewZeland
+
+// let whoWillWinToday = "India";
+// if (true){
+//     let whoWillWinToday = "NewZeland";
+//     console.log(whoWillWinToday)
+// }
+// console.log(whoWillWinToday)
+
+// // o/p: NewZeland
+// //      India
+
+const whoWillWinToday = "India";
+if (true){
+    const whoWillWinToday = "NewZeland";
+    console.log(whoWillWinToday)
+}
+console.log(whoWillWinToday)
+
+// o/p: NewZeland
+//      India
