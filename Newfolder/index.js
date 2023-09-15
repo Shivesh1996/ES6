9340c45838e775380babdf99736ddaca809f9a18
Shivesh Pandey <shiveshpandey1996@gmail.com>
2023-09-15 16:51:56 +0530

    Add files via upload

---
+++ Newfolder/index.js
@@ -0,0 +1,117 @@
+// let poo = "Soni"
+// console.log(poo)
+// var pooja = "jii"
+// console.log(pooja)
+// const aviral = "chuuuuu"
+// console.log(aviral)
+
+// var name = "Shivesh";
+// name = "Pandey";
+// console.log(name);
+
+// let name = "Shivesh";
+// name = "Pandey";
+// console.log(name);
+
+// const name = "Shivesh";
+// name = "Pandey";
+// console.log(name);
+
+////block scope
+// function modeJS(love){
+//     if (love){
+//         let name  = "Shivesh"
+//         const sname = "Pandey"
+//         console.log("My name is "+name+" "+sname)
+//     }
+// }
+// modeJS(true)
+ 
+// function modeJS(love){
+//     if (love){
+//         let name  = "Shivesh"
+//         const sname = "Pandey"
+//     }
+//     console.log("My name is "+name+" "+sname) //error: sname is not defined
+// }
+// modeJS(true)
+ 
+
+// //function scope
+// function modeJS(love){
+//     if (love){
+//         var name  = "Shivesh"
+//         var sname = "Pandey"
+//     }
+//     console.log("My name is "+name+" "+sname)
+// }
+// modeJS(true)
+
+// var whoWillWinToday = "India";
+// if (true){
+//     var whoWillWinToday = "NewZeland";
+//     console.log(whoWillWinToday)
+// }
+// console.log(whoWillWinToday)
+
+// // o/p: NewZeland
+// //      NewZeland
+
+// let whoWillWinToday = "India";
+// if (true){
+//     let whoWillWinToday = "NewZeland";
+//     console.log(whoWillWinToday)
+// }
+// console.log(whoWillWinToday)
+
+// // o/p: NewZeland
+// //      India
+
+// const whoWillWinToday = "India";
+// if (true){
+//     const whoWillWinToday = "NewZeland";
+//     console.log(whoWillWinToday)
+// }
+// console.log(whoWillWinToday)
+
+// o/p: NewZeland
+//      India
+
+//-------------------------------------------------------------------------------------------//
+//Video -> 3
+// //es5
+// console.log("My name is Shivesh." + "My age is 27")
+// //o/p: My name is Shivesh.My age is 27
+
+// //es6
+// console.log(`My name is Shivesh.My age is 27`)
+// //o/p: My name is Shivesh.My age is 27
+
+
+// let firstName = "Shivesh"
+// let lastName = "Pandey"
+// //es5
+// console.log("My first name is " + firstName + ". My last name is " + lastName + ".")
+// //o/p: My first name is Shivesh. My last name is Pandey.
+
+// //es6
+// console.log(`My first name is ${firstName}. My last name is ${lastName}.`)
+// //o/p: My first name is Shivesh. My last name is Pandey.
+
+// //we can add two expressions as well
+// let a = 20;
+// let b = 30;
+// //es5
+// console.log("Fifty is " + (a + b) + " and\nnot " + (2 * a + b) + ".");
+// //o/p:Fifty is 50 and
+// //    not 70.
+
+// //es6
+// console.log(`Fifty is ${a + b} and
+// not ${2 * a + b}.`);
+// //o/p:Fifty is 50 and
+// //    not 70.
+
+
+//-------------------------------------------------------------------------------------------//
+//Video -> 4
